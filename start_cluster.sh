33dcbe50e41ba17a0e3a2d047ce8ad8652217c79
C Peter Jerold Leslie <jeroldleslie@gmail.com>
2017-04-03 23:57:04 +0530

    m

--- start_cluster.sh
+++ start_cluster.sh
@@ -4,8 +4,8 @@ ssh sneha@zookeeper-1 '/opt/zookeeper/bin/zkServer.sh start'
 
 sleep 3
 echo "Start 3 node Kafka cluster"
-ssh sneha@kafka-1 '/opt/kafka/bin/kafka-server-start.sh -daemon /opt/kafka/config/server.properties'
-ssh sneha@kafka-2 '/opt/kafka/bin/kafka-server-start.sh -daemon /opt/kafka/config/server.properties'
-ssh sneha@kafka-3 '/opt/kafka/bin/kafka-server-start.sh -daemon /opt/kafka/config/server.properties'
+ssh sneha@kafka-storm-1 '/opt/kafka/bin/kafka-server-start.sh -daemon /opt/kafka/config/server.properties'
+ssh sneha@kafka-storm-2 '/opt/kafka/bin/kafka-server-start.sh -daemon /opt/kafka/config/server.properties'
+ssh sneha@kafka-storm-3 '/opt/kafka/bin/kafka-server-start.sh -daemon /opt/kafka/config/server.properties'
 
 
