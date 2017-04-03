dde5d5c35224a57317a35b7f4f3d87d6810d7067
C Peter Jerold Leslie <jeroldleslie@gmail.com>
2017-04-03 21:03:52 +0530

    m

--- restart.sh
+++
@@ -1,5 +0,0 @@
-docker rm -f $(docker ps -aq)
-docker run -d -p 22 -p 2181 --privileged -h zookeeper-1 --name zookeeper-1 zookeeper-1:sneha
-docker run -d -p 22 -p 9092 --privileged -h kafka-1 --name kafka-1 kafka-1:sneha
-docker run -d -p 22 -p 9092 --privileged -h kafka-2 --name kafka-2 kafka-2:sneha
-docker run -d -p 22 -p 2181 --privileged -h kafka-3 --name kafka-3 kafka-3:sneha
--- start_cluster.sh
+++ start_cluster.sh
@@ -1,9 +1,9 @@
-sleep 5
+
 echo "Start Zookeeper"
 docker exec -it zookeeper-1 /opt/zookeeper/bin/zkServer.sh start
 
 sleep 5
 echo "Start 3 Kafka"
-docker exec -it kafka-1 /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties
-docker exec -it kafka-2 /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties
-docker exec -it kafka-3 /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties
+docker exec -it -u sneha kafka-1 /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties
+docker exec -it -u sneha kafka-2 /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties
+docker exec -it -u sneha kafka-3 /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties
