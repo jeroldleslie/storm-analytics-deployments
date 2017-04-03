94f04d68d9cb52628ecff776cd170d4a9d34a86c
C Peter Jerold Leslie <jeroldleslie@gmail.com>
2017-04-04 02:43:12 +0530

    m

--- start_cluster.sh
+++ start_cluster.sh
@@ -20,3 +20,15 @@ ssh sneha@kafka-storm-2 'nohup /opt/storm/bin/storm supervisor&' &
 echo 'Storm supervisor started'
 ssh sneha@kafka-storm-3 'nohup /opt/storm/bin/storm supervisor&' &
 echo 'Storm supervisor started'
+
+
+echo 'Create kafka topic'
+ssh sneha@kafka-storm-1 '/opt/kafka/bin/kafka-topics.sh --create --zookeeper zookeeper-1:2181 --replication-factor 2 --partitions 2 --topic storm-events'
+echo 'storm-events topic created'
+
+echo 'Check kafka topic'
+ssh sneha@kafka-storm-1 '/opt/kafka/bin/kafka-topics.sh --list --zookeeper zookeeper-1:2181'
+
+
+#ssh sneha@kafka-storm-1  '/opt/storm/bin/storm jar /opt/storm-events-analytics-0.0.1-SNAPSHOT-jar-with-dependencies.jar com.stormevents.analytics.StormEventAnalytics zookeeper-1:2181 storm-events /brokers prod mongodb://172.17.0.1:27017/storm-events'
+ 
