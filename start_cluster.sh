dba0e7e29430ca602e426caaa6118fb4fdb00ab7
C Peter Jerold Leslie <jeroldleslie@gmail.com>
2017-04-03 23:05:43 +0530

    m

--- start_cluster.sh
+++ start_cluster.sh
@@ -1,9 +1,12 @@
 
 echo "Start Zookeeper"
-docker exec -it zookeeper-1 /opt/zookeeper/bin/zkServer.sh start
+docker exec -it -u sneha zookeeper-1 /opt/zookeeper/bin/zkServer.sh start
 
 sleep 5
 echo "Start 3 Kafka"
-docker exec -it kafka-1 /opt/kafka/bin/kafka-server-start.sh -daemon /opt/kafka/config/server.properties
-docker exec -it kafka-2 /opt/kafka/bin/kafka-server-start.sh -daemon /opt/kafka/config/server.properties
-docker exec -it kafka-3 /opt/kafka/bin/kafka-server-start.sh -daemon /opt/kafka/config/server.properties
+docker exec -it -u sneha kafka-1 /opt/kafka/bin/kafka-server-start.sh -daemon /opt/kafka/config/server.properties
+docker exec -it -u sneha kafka-2 /opt/kafka/bin/kafka-server-start.sh -daemon /opt/kafka/config/server.properties
+docker exec -it -u sneha kafka-3 /opt/kafka/bin/kafka-server-start.sh -daemon /opt/kafka/config/server.properties
+
+
+
