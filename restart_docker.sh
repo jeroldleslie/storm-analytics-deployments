01db283e28733a86b4842370f34618545436b514
C Peter Jerold Leslie <jeroldleslie@gmail.com>
2017-04-04 02:48:47 +0530

    m

--- restart_docker.sh
+++ restart_docker.sh
@@ -1,6 +1,6 @@
 docker rm -f $(docker ps -aq)
 docker run -d -p 22 -p 2181:2181 --privileged -h zookeeper-1 --name zookeeper-1 zookeeper-1:sneha
 docker run -d -p 22 -p 9092:9092 --privileged -h kafka-storm-1 --name kafka-storm-1 kafka-storm-1:sneha
-docker run -d -p 22 -p 9092:9093 --privileged -h kafka-storm-2 --name kafka-storm-2 kafka-storm-2:sneha
-docker run -d -p 22 -p 9092:9094 --privileged -h kafka-storm-3 --name kafka-storm-3 kafka-storm-3:sneha
+docker run -d -p 22 -p 9093:9092 --privileged -h kafka-storm-2 --name kafka-storm-2 kafka-storm-2:sneha
+docker run -d -p 22 -p 9094:9092 --privileged -h kafka-storm-3 --name kafka-storm-3 kafka-storm-3:sneha
 
