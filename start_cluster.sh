9eac5fa5fc5400e27ba91c53fcc9bd86cb8d1f45
C Peter Jerold Leslie <jeroldleslie@gmail.com>
2017-04-04 15:57:36 +0530

    m

--- start_cluster.sh
+++ start_cluster.sh
@@ -24,8 +24,8 @@ ssh sneha@kafka-storm-3 'nohup /opt/storm/bin/storm supervisor&' &
 echo 'Storm supervisor started'
 
 
-echo 'Sleeping for 10 seconds to allow all services and clusters to start...'
-sleep 10
+echo 'Sleeping for 20 seconds to allow all services and clusters to start...'
+sleep 20
 echo 'Create kafka topic'
 ssh sneha@kafka-storm-1 '/opt/kafka/bin/kafka-topics.sh --create --zookeeper zookeeper-1:2181 --replication-factor 2 --partitions 2 --topic storm-events'
 echo 'storm-events topic created'
@@ -34,6 +34,14 @@ echo 'Check kafka topic'
 ssh sneha@kafka-storm-1 '/opt/kafka/bin/kafka-topics.sh --list --zookeeper zookeeper-1:2181'
 
 
+
 #ssh sneha@kafka-storm-1  '/opt/storm/bin/storm jar /opt/storm-events-analytics-0.0.1-SNAPSHOT-jar-with-dependencies.jar com.stormevents.analytics.StormEventAnalytics zookeeper-1:2181 storm-events /brokers prod mongodb://172.17.0.1:27017/storm-events'
  
- #bin/kafka-console-producer.sh --broker-list kafka-storm:9092 --topic storm-events
+
+ bin/kafka-topics.sh --create --zookeeper zookeeper-1:2181 --replication-factor 1 --partitions 1 --topic test
+ #bin/kafka-console-producer.sh --broker-list kafka-storm-1:9092 --topic test
+
+
+ bin/kafka-console-producer.sh --broker-list kafka-storm-1:9092 --topic test
+
+ bin/kafka-console-consumer.sh --bootstrap-server kafka-storm-1:9092 --topic test --from-beginning
