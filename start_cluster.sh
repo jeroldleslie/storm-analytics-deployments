8469388cb70670dbf7c044af0653e8b1f93b6cfc
C Peter Jerold Leslie <jeroldleslie@gmail.com>
2017-04-04 12:11:01 +0530

    m

--- start_cluster.sh
+++ start_cluster.sh
@@ -1,9 +1,11 @@
 
-echo "Start Zookeeper"
+echo "Starting Zookeeper..."
 ssh sneha@zookeeper-1 '/opt/zookeeper/bin/zkServer.sh start'
+echo 'zookeeper-1 started'
 
+echo 'Sleeping for 3 seconds to allow zookeeper to start...'
 sleep 3
-echo "Start Kafka cluster"
+echo "Starting Kafka cluster..."
 ssh sneha@kafka-storm-1 '/opt/kafka/bin/kafka-server-start.sh -daemon /opt/kafka/config/server.properties'
 echo 'kafka-1 started'
 ssh sneha@kafka-storm-2 '/opt/kafka/bin/kafka-server-start.sh -daemon /opt/kafka/config/server.properties'
@@ -11,7 +13,7 @@ echo 'kafka-2 started'
 ssh sneha@kafka-storm-3 '/opt/kafka/bin/kafka-server-start.sh -daemon /opt/kafka/config/server.properties'
 echo 'kafka-3 started'
 
-echo "Start Storm Cluster"
+echo "Starting Storm Cluster..."
 ssh sneha@kafka-storm-1 'nohup /opt/storm/bin/storm nimbus &' &
 echo 'Storm nimbus started'
 ssh sneha@kafka-storm-1 'nohup /opt/storm/bin/storm ui &' &
@@ -22,6 +24,8 @@ ssh sneha@kafka-storm-3 'nohup /opt/storm/bin/storm supervisor&' &
 echo 'Storm supervisor started'
 
 
+echo 'Sleeping for 10 seconds to allow all services and clusters to start...'
+sleep 10
 echo 'Create kafka topic'
 ssh sneha@kafka-storm-1 '/opt/kafka/bin/kafka-topics.sh --create --zookeeper zookeeper-1:2181 --replication-factor 2 --partitions 2 --topic storm-events'
 echo 'storm-events topic created'
@@ -32,3 +36,4 @@ ssh sneha@kafka-storm-1 '/opt/kafka/bin/kafka-topics.sh --list --zookeeper zooke
 
 #ssh sneha@kafka-storm-1  '/opt/storm/bin/storm jar /opt/storm-events-analytics-0.0.1-SNAPSHOT-jar-with-dependencies.jar com.stormevents.analytics.StormEventAnalytics zookeeper-1:2181 storm-events /brokers prod mongodb://172.17.0.1:27017/storm-events'
  
+ #bin/kafka-console-producer.sh --broker-list kafka-storm:9092 --topic storm-events
