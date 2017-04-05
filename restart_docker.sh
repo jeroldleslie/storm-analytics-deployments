3892e49ec65415692bae1fc8626837debeed2558
C Peter Jerold Leslie <jeroldleslie@gmail.com>
2017-04-05 17:25:50 +0530

    m

--- restart_docker.sh
+++ restart_docker.sh
@@ -1,3 +1,4 @@
+docker stop $(docker ps -aq)
 docker rm -f $(docker ps -aq)
 docker run -d -p 22 -p 2181 --privileged -h zookeeper-1 --name zookeeper-1 zoo-kafka-storm-base:sneha
 docker run -d -p 22 -p 9092 --privileged -h kafka-storm-1 --name kafka-storm-1 zoo-kafka-storm-base:sneha
