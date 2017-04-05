1dbcfefeafb29a09d6d04d4c2862c3a73b7cedb1
C Peter Jerold Leslie <jeroldleslie@gmail.com>
2017-04-05 17:17:50 +0530

    m

--- start_cluster.sh
+++ start_cluster.sh
@@ -1,27 +1,27 @@
 
 echo "Starting Zookeeper..."
 ssh sneha@zookeeper-1 '/opt/zookeeper/bin/zkServer.sh start'
-echo 'zookeeper-1 started'
+echo 'zookeeper in zookeeper-1 started'
 
 echo 'Sleeping for 3 seconds to allow zookeeper to start...'
 sleep 3
 echo "Starting Kafka cluster..."
-ssh sneha@kafka-storm-1 '/opt/kafka/bin/kafka-server-start.sh -daemon /opt/kafka/config/server.properties'
-echo 'kafka-1 started'
-ssh sneha@kafka-storm-2 '/opt/kafka/bin/kafka-server-start.sh -daemon /opt/kafka/config/server.properties'
-echo 'kafka-2 started'
-ssh sneha@kafka-storm-3 '/opt/kafka/bin/kafka-server-start.sh -daemon /opt/kafka/config/server.properties'
-echo 'kafka-3 started'
+ssh sneha@kafka-storm-1 '/opt/kafka/bin/kafka-server-start.sh -daemon /opt/kafka/config/server-1.properties'
+echo 'kafka in kafka-storm-1 started'
+ssh sneha@kafka-storm-2 '/opt/kafka/bin/kafka-server-start.sh -daemon /opt/kafka/config/server-2.properties'
+echo 'kafka in kafka-storm-2 started'
+ssh sneha@kafka-storm-3 '/opt/kafka/bin/kafka-server-start.sh -daemon /opt/kafka/config/server-3.properties'
+echo 'kafka in kafka-storm-3 started'
 
 echo "Starting Storm Cluster..."
 ssh sneha@kafka-storm-1 'nohup /opt/storm/bin/storm nimbus &' &
-echo 'Storm nimbus started'
+echo 'Storm nimbus in afka-storm-1 started'
 ssh sneha@kafka-storm-1 'nohup /opt/storm/bin/storm ui &' &
-echo 'Storm ui started'
+echo 'Storm ui in afka-storm-1 started'
 ssh sneha@kafka-storm-2 'nohup /opt/storm/bin/storm supervisor&' &
-echo 'Storm supervisor started'
+echo 'Storm supervisor in kafka-storm-2 started'
 ssh sneha@kafka-storm-3 'nohup /opt/storm/bin/storm supervisor&' &
-echo 'Storm supervisor started'
+echo 'Storm supervisor in kafka-storm-3 started'
 
 
 #echo 'Sleeping for 30 seconds to allow all services and clusters to start...'
